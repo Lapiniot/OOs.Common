53cd0f65d8db961edfc8bbb24b9048e6eab768f2
Lapiniot <aossss@gmail.com>
2018-05-01 14:08:40 +0300

    TaskExtensions refactored

--- System.Common/Threading/TaskExtensions.cs
+++ System.Common/Threading/TaskExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
-using static System.Threading.Tasks.Task;
+using static System.Threading.Tasks.TaskContinuationOptions;
+using static System.Threading.Tasks.TaskScheduler;
 
 namespace System.Threading
 {
@@ -11,26 +12,10 @@ namespace System.Threading
         /// <typeparam name="T">Result type</typeparam>
         /// <param name="task">Task to be awaited</param>
         /// <param name="cancellationToken">Cancellation token to signal about external cancellation</param>
-        /// <returns>
-        /// Wrapper task that either containes <paramref name="task" /> after completion or cancelled proxy task if external
-        /// cancellation has been signaled via <paramref name="cancellationToken" />
-        /// </returns>
-        public static async Task<Task<T>> WaitAsync<T>(this Task<T> task, CancellationToken cancellationToken)
+        /// <returns>Wrapper task</returns>
+        public static Task<T> WaitAsync<T>(this Task<T> task, CancellationToken cancellationToken)
         {
-            var completionSource = new TaskCompletionSource<T>();
-
-            var registration = completionSource.Bind(cancellationToken);
-
-            try
-            {
-                return await WhenAny(completionSource.Task, task).ConfigureAwait(false);
-            }
-            finally
-            {
-                registration.Dispose();
-
-                completionSource.TrySetResult(default);
-            }
+            return task.IsCompleted ? task : task.ContinueWith(t => t.Result, cancellationToken, ExecuteSynchronously, Default);
         }
 
         /// <summary>
@@ -38,62 +23,10 @@ namespace System.Threading
         /// </summary>
         /// <param name="task">Task to be awaited</param>
         /// <param name="cancellationToken">Cancellation token to signal about external cancellation</param>
-        /// <returns>
-        /// Wrapper task that either containes <paramref name="task" /> after completion or cancelled proxy task if external
-        /// cancellation has been signaled via <paramref name="cancellationToken" />
-        /// </returns>
-        public static async Task<Task> WaitAsync(this Task task, CancellationToken cancellationToken)
+        /// <returns>Wrapper task</returns>
+        public static Task WaitAsync(this Task task, CancellationToken cancellationToken)
         {
-            var completionSource = new TaskCompletionSource<bool>();
-
-            var registration = completionSource.Bind(cancellationToken);
-
-            try
-            {
-                return await WhenAny(completionSource.Task, task).ConfigureAwait(false);
-            }
-            finally
-            {
-                registration.Dispose();
-
-                completionSource.TrySetResult(default);
-            }
-        }
-
-        /// <summary>
-        /// Adds non-blocking cancellable waiting support to the <paramref name="task" /> that doesn't support cancellation
-        /// natively
-        /// and unwraps its result on completion
-        /// </summary>
-        /// <param name="task">Task to be awaited</param>
-        /// <param name="cancellationToken">Cancellation token to signal about external cancellation</param>
-        /// <returns>
-        /// Either original <paramref name="task" /> on completion or cancelled proxy task if cancellation has been
-        /// requested via <paramref name="cancellationToken" />
-        /// </returns>
-        /// <exception cref="TaskCanceledException">If cancellation requested via <paramref name="cancellationToken" /></exception>
-        public static async Task WaitAndUnwrapAsync(this Task task, CancellationToken cancellationToken)
-        {
-            await (await task.WaitAsync(cancellationToken).ConfigureAwait(false)).ConfigureAwait(false);
-        }
-
-
-        /// <summary>
-        /// Adds non-blocking cancellable waiting support to the <paramref name="task" /> that doesn't support cancellation
-        /// natively
-        /// and unwraps its result on completion
-        /// </summary>
-        /// <typeparam name="T">Awaitable task result type</typeparam>
-        /// <param name="task">Task to be awaited</param>
-        /// <param name="cancellationToken">Cancellation token to signal about external cancellation</param>
-        /// <returns>
-        /// Either original <paramref name="task" /> on completion or cancelled proxy task if cancellation has been
-        /// requested via <paramref name="cancellationToken" />
-        /// </returns>
-        /// <exception cref="TaskCanceledException">If cancellation requested via <paramref name="cancellationToken" /></exception>
-        public static async Task<T> WaitAndUnwrapAsync<T>(this Task<T> task, CancellationToken cancellationToken)
-        {
-            return await (await task.WaitAsync(cancellationToken).ConfigureAwait(false)).ConfigureAwait(false);
+            return task.IsCompleted ? task : task.ContinueWith(t => { t.GetAwaiter().GetResult(); }, cancellationToken, ExecuteSynchronously, Default);
         }
     }
 }
