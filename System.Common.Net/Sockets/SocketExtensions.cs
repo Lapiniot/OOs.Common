6ecdb1f9b1371b75cad9bd3ea142e2788276525b
Lapiniot <aossss@gmail.com>
2018-05-24 10:27:07 +0300

    big project structure refactoring in order to separate concerns and reorganize namespace relations

---
+++ System.Common.Net/Sockets/SocketExtensions.cs
@@ -0,0 +1,140 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Net.Sockets
+{
+    public static class SocketExtensions
+    {
+        public delegate IAsyncResult AsyncBeginHandler(byte[] bytes, int offset, int size, SocketFlags flags, AsyncCallback callback, object state);
+
+        public delegate T AsyncEndHandler<out T>(IAsyncResult asyncResult);
+
+        public static async Task<T> FromAsync<T>(Socket socket, byte[] bytes, int offset, int size, IPEndPoint remotEndPoint,
+            AsyncBeginHandler beginMethod, AsyncEndHandler<T> endMethod, CancellationToken cancellationToken)
+        {
+            var completionSource = new TaskCompletionSource<T>();
+
+            using(completionSource.Bind(cancellationToken))
+            {
+                try
+                {
+                    var asyncState = new AsyncStateBag<T>(socket, remotEndPoint, completionSource, endMethod);
+
+                    beginMethod(bytes, offset, size, SocketFlags.None, asyncState.AsyncCallback, asyncState);
+                }
+                catch(Exception exception)
+                {
+                    completionSource.TrySetException(exception);
+                }
+
+                return await completionSource.Task.ConfigureAwait(false);
+            }
+        }
+
+        private class AsyncStateBag<T>
+        {
+            public AsyncStateBag(Socket socket, IPEndPoint endPoint, TaskCompletionSource<T> completionSource, AsyncEndHandler<T> endMethod) =>
+                (Socket, EndPoint, CompletionSource, EndMethod) = (socket, endPoint, completionSource, endMethod);
+
+            private AsyncEndHandler<T> EndMethod { get; }
+            private TaskCompletionSource<T> CompletionSource { get; }
+            public Socket Socket { get; }
+            public IPEndPoint EndPoint { get; }
+
+            public void AsyncCallback(IAsyncResult ar)
+            {
+                try
+                {
+                    CompletionSource.TrySetResult(EndMethod(ar));
+                }
+                catch(Exception exception)
+                {
+                    CompletionSource.TrySetException(exception);
+                }
+            }
+        }
+
+        #region SendAsync overloads
+
+        public static Task<int> SendAsync(this Socket socket, byte[] bytes, int offset, int size, CancellationToken cancellationToken)
+        {
+            return FromAsync(socket, bytes, offset, size, null, socket.BeginSend, socket.EndSend, cancellationToken);
+        }
+
+        public static Task<int> SendAsync(this Socket socket, byte[] bytes, CancellationToken cancellationToken)
+        {
+            return SendAsync(socket, bytes, 0, bytes.Length, cancellationToken);
+        }
+
+        #endregion
+
+        #region SendToAsync overloads
+
+        public static Task<int> SendToAsync(this Socket socket, byte[] bytes, int offset, int size, IPEndPoint remoteEndPoint, CancellationToken cancellationToken)
+        {
+            return FromAsync(socket, bytes, offset, size, remoteEndPoint, BeginSendTo, EndSendTo, cancellationToken);
+        }
+
+        public static Task<int> SendToAsync(this Socket socket, byte[] bytes, IPEndPoint remoteEndPoint, CancellationToken cancellationToken)
+        {
+            return SendToAsync(socket, bytes, 0, bytes.Length, remoteEndPoint, cancellationToken);
+        }
+
+        private static IAsyncResult BeginSendTo(byte[] bytes, int offset, int size, SocketFlags flags, AsyncCallback callback, object state)
+        {
+            var asyncState = (AsyncStateBag<int>)state;
+
+            return asyncState.Socket.BeginSendTo(bytes, offset, size, flags, asyncState.EndPoint, callback, state);
+        }
+
+        private static int EndSendTo(IAsyncResult asyncResult)
+        {
+            return ((AsyncStateBag<int>)asyncResult.AsyncState).Socket.EndSendTo(asyncResult);
+        }
+
+        #endregion
+
+        #region ReceiveAsync overloads
+
+        public static Task<int> ReceiveAsync(this Socket socket, byte[] bytes, int offset, int size, CancellationToken cancellationToken)
+        {
+            return FromAsync(socket, bytes, offset, size, null, socket.BeginReceive, socket.EndReceive, cancellationToken);
+        }
+
+        public static Task<int> ReceiveAsync(this Socket socket, byte[] bytes, CancellationToken cancellationToken)
+        {
+            return ReceiveAsync(socket, bytes, 0, bytes.Length, cancellationToken);
+        }
+
+        #endregion
+
+        #region ReceiveFromAsync overloads 
+
+        public static Task<(int Size, IPEndPoint RemoteEndPoint)> ReceiveFromAsync(this Socket socket, byte[] bytes, int offset, int size,
+            IPEndPoint endPoint, CancellationToken cancellationToken)
+        {
+            return FromAsync(socket, bytes, offset, size, endPoint, BeginReceiveFrom, EndReceiveFrom, cancellationToken);
+        }
+
+        public static Task<(int Size, IPEndPoint RemoteEndPoint)> ReceiveFromAsync(this Socket socket, byte[] bytes, IPEndPoint endPoint, CancellationToken cancellationToken)
+        {
+            return ReceiveFromAsync(socket, bytes, 0, bytes.Length, endPoint, cancellationToken);
+        }
+
+        private static IAsyncResult BeginReceiveFrom(byte[] bytes, int offset, int size, SocketFlags flags, AsyncCallback callback, object state)
+        {
+            var asyncState = (AsyncStateBag<(int, IPEndPoint)>)state;
+            EndPoint e = asyncState.EndPoint;
+            return asyncState.Socket.BeginReceiveFrom(bytes, offset, size, flags, ref e, callback, state);
+        }
+
+        private static (int Size, IPEndPoint RemoteEndPoint) EndReceiveFrom(IAsyncResult result)
+        {
+            var asyncState = (AsyncStateBag<(int, IPEndPoint)>)result.AsyncState;
+            EndPoint endPoint = asyncState.EndPoint;
+            return (asyncState.Socket.EndReceiveFrom(result, ref endPoint), (IPEndPoint)endPoint);
+        }
+
+        #endregion
+    }
+}
---
+++ System.Common.Net/Sockets/Sockets.cs
@@ -0,0 +1,57 @@
+﻿using static System.Net.Sockets.AddressFamily;
+using static System.Net.Sockets.SocketType;
+
+namespace System.Net.Sockets
+{
+    public delegate Socket CreateSocketFactory();
+
+    public delegate Socket CreateConnectedSocketFactory(IPEndPoint remoteEndPoint);
+
+    public static class Sockets
+    {
+        private class UdpSocket : Socket
+        {
+            public UdpSocket() : base(InterNetwork, Dgram, ProtocolType.Udp)
+            {
+            }
+        }
+
+        public static class Udp
+        {
+            public static Socket Broadcast()
+            {
+                var socket = new UdpSocket();
+                socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+                return socket;
+            }
+
+            public static Socket Connected(IPEndPoint endpoint)
+            {
+                var socket = new UdpSocket();
+                socket.Connect(endpoint);
+                return socket;
+            }
+
+            public static class Multicast
+            {
+                public static Socket Sender()
+                {
+                    return new UdpSocket();
+                }
+
+                public static Socket Listener(IPEndPoint group)
+                {
+                    var socket = new UdpSocket();
+
+                    socket.Bind(new IPEndPoint(IPAddress.Any, group.Port));
+
+                    socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                    socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(group.Address));
+                    socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 64);
+
+                    return socket;
+                }
+            }
+        }
+    }
+}
