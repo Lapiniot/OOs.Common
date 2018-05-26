e82686298020621c2516ed3f95cc9e7afa2c25fe
Lapiniot <aossss@gmail.com>
2018-05-26 21:04:05 +0300

    1. Improved handling of multicast on multihomed machines 2. User-Agent field add to SSDP search message

--- System.Common.Net/Sockets/Sockets.cs
+++ System.Common.Net/Sockets/Sockets.cs
@@ -1,4 +1,8 @@
-﻿using static System.Net.Sockets.AddressFamily;
+﻿using System.Net.NetworkInformation;
+using static System.Net.NetworkInformation.OperationalStatus;
+using static System.Net.Sockets.AddressFamily;
+using static System.Net.Sockets.SocketOptionLevel;
+using static System.Net.Sockets.SocketOptionName;
 using static System.Net.Sockets.SocketType;
 
 namespace System.Net.Sockets
@@ -9,9 +13,14 @@ namespace System.Net.Sockets
 
     public static class Sockets
     {
+        public static class EndPoint
+        {
+            public static readonly IPEndPoint Any = new IPEndPoint(IPAddress.Any, 0);
+        }
+
         private class UdpSocket : Socket
         {
-            public UdpSocket() : base(InterNetwork, Dgram, ProtocolType.Udp)
+            public UdpSocket(AddressFamily addressFamily = InterNetwork) : base(addressFamily, Dgram, ProtocolType.Udp)
             {
             }
         }
@@ -36,18 +45,31 @@ namespace System.Net.Sockets
             {
                 public static Socket Sender()
                 {
-                    return new UdpSocket();
+                    var udpSocket = new UdpSocket();
+
+                    foreach(var i in NetworkInterface.GetAllNetworkInterfaces())
+                    {
+                        if(i.SupportsMulticast && i.OperationalStatus == Up && i.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                        {
+                            var ipProperties = i.GetIPProperties();
+                            var iPv4Properties = ipProperties.GetIPv4Properties();
+                            udpSocket.SetSocketOption(IP, MulticastInterface, IPAddress.HostToNetworkOrder(iPv4Properties.Index));
+                        }
+                    }
+
+                    return udpSocket;
                 }
 
                 public static Socket Listener(IPEndPoint group)
                 {
-                    var socket = new UdpSocket();
+                    var socket = new UdpSocket {ExclusiveAddressUse = false};
+
+                    socket.SetSocketOption(SocketOptionLevel.Socket, ReuseAddress, true);
 
                     socket.Bind(new IPEndPoint(IPAddress.Any, group.Port));
 
-                    socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-                    socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(group.Address));
-                    socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 64);
+                    socket.SetSocketOption(IP, AddMembership, new MulticastOption(group.Address));
+                    socket.SetSocketOption(IP, MulticastTimeToLive, 64);
 
                     return socket;
                 }
