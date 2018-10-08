9ae31b4983dc8025a046b97c508c226315b880cd
Lapiniot <aossss@gmail.com>
2018-10-08 13:06:15 +0300

    HostNotFoundException added

---
+++ System.Common.Net/Transports/Exceptions/HostNotFoundException.cs
@@ -0,0 +1,17 @@
+namespace System.Net.Transports.Exceptions
+{
+    public class HostNotFoundException : TransportException
+    {
+        protected internal const string HostNotFound = "Host not found.";
+
+        public HostNotFoundException(Exception innerException) :
+            base(HostNotFound, innerException)
+        {
+        }
+
+        public HostNotFoundException(string message, Exception innerException) :
+            base(message, innerException)
+        {
+        }
+    }
+}
