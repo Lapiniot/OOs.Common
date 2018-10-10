529b208b8533eafeb76113a2063425f5b7b25181
Lapiniot <aossss@gmail.com>
2018-10-10 12:13:18 +0300

    base abstract ConnectionListener added

--- System.Common.Net/ConnectionListener.cs
+++ System.Common.Net/ConnectionListener.cs
@@ -1,8 +1,16 @@
-﻿namespace System.Net
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Net
 {
-    public abstract class ConnectionListener : IObservable<INetworkTransport>, IDisposable
+    public abstract class ConnectionListener : IConnectionListener
     {
         private readonly ObserversContainer<INetworkTransport> observers = new ObserversContainer<INetworkTransport>();
+        private readonly object syncRoot = new object();
+        private CancellationTokenSource cancellationTokenSource;
+        private bool isListening;
+
+        public bool IsListening => isListening;
 
         public void Dispose()
         {
@@ -15,14 +23,60 @@
             return observers.Subscribe(observer);
         }
 
-        public abstract void Start();
+        public void Start()
+        {
+            if(!isListening)
+            {
+                lock(syncRoot)
+                {
+                    if(!isListening)
+                    {
+                        OnStartListening();
+                        isListening = true;
+                        cancellationTokenSource = new CancellationTokenSource();
+                        Task.Run(StartAcceptingConnectionsAsync);
+                    }
+                }
+            }
+        }
 
-        public abstract void Stop();
+        public void Stop()
+        {
+            if(isListening)
+            {
+                lock(syncRoot)
+                {
+                    if(isListening)
+                    {
+                        OnStopListening();
+                        isListening = false;
+                    }
+                }
+            }
+        }
+
+        private async Task StartAcceptingConnectionsAsync()
+        {
+            var token = cancellationTokenSource.Token;
+
+            while(!token.IsCancellationRequested)
+            {
+                var networkTransport = await AcceptAsync(token).ConfigureAwait(false);
+                observers.Notify(networkTransport);
+            }
+        }
+
+        protected abstract Task<INetworkTransport> AcceptAsync(CancellationToken cancellationToken);
+
+        protected abstract void OnStartListening();
+
+        protected abstract void OnStopListening();
 
         protected virtual void Dispose(bool disposing)
         {
             if(disposing)
             {
+                cancellationTokenSource?.Dispose();
                 observers?.Dispose();
             }
         }
--- System.Common.Net/Listeners/TcpSocketConnectionListener.cs
+++ System.Common.Net/Listeners/TcpSocketConnectionListener.cs
@@ -1,22 +1,73 @@
-﻿namespace System.Net.Listeners
+﻿using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Net.Listeners
 {
-    public class TcpSocketConnectionListener : ConnectionListener
+    public sealed class TcpSocketConnectionListener : ConnectionListener
     {
         private readonly IPEndPoint ipEndPoint;
+        private readonly int maxConnections;
+        private Socket socket;
 
-        public TcpSocketConnectionListener(IPEndPoint ipEndPoint)
+        public TcpSocketConnectionListener(IPEndPoint ipEndPoint, int maxConnections = 100)
         {
             this.ipEndPoint = ipEndPoint;
+            this.maxConnections = maxConnections;
         }
 
-        public override void Start()
+        protected override async Task<INetworkTransport> AcceptAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            using(cancellationToken.Register(socket.Close))
+            {
+                Socket connectedSocket = null;
+                try
+                {
+                    connectedSocket = await socket.AcceptAsync().ConfigureAwait(false);
+
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    return new TcpSocketsTransportWrapper(connectedSocket);
+                }
+                catch
+                {
+                    connectedSocket?.Close();
+                    throw;
+                }
+            }
         }
 
-        public override void Stop()
+        protected override void OnStartListening()
         {
-            throw new NotImplementedException();
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(ipEndPoint);
+            socket.Listen(maxConnections);
+        }
+
+        protected override void OnStopListening()
+        {
+            socket.Close();
+            socket = null;
+        }
+
+        private class TcpSocketsTransportWrapper : INetworkTransport
+        {
+            private readonly Socket socket;
+
+            public TcpSocketsTransportWrapper(Socket socket)
+            {
+                this.socket = socket;
+            }
+
+            public Task<int> SendAsync(Memory<byte> buffer, CancellationToken cancellationToken)
+            {
+                return socket.SendAsync(buffer, cancellationToken);
+            }
+
+            public Task<int> ReceiveAsync(Memory<byte> buffer, CancellationToken cancellationToken)
+            {
+                return socket.ReceiveAsync(buffer, cancellationToken);
+            }
         }
     }
 }
