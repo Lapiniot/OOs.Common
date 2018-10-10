5ebc2c2b164b0bc7ba2ce3e58feba9dad60a5f92
Lapiniot <aossss@gmail.com>
2018-10-10 11:19:16 +0300

    ConnectionListener abstraction introduced

---
+++ System.Common.Net/ConnectionListener.cs
@@ -0,0 +1,30 @@
+﻿namespace System.Net
+{
+    public abstract class ConnectionListener : IObservable<INetworkTransport>, IDisposable
+    {
+        private readonly ObserversContainer<INetworkTransport> observers = new ObserversContainer<INetworkTransport>();
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public IDisposable Subscribe(IObserver<INetworkTransport> observer)
+        {
+            return observers.Subscribe(observer);
+        }
+
+        public abstract void Start();
+
+        public abstract void Stop();
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                observers?.Dispose();
+            }
+        }
+    }
+}
---
+++ System.Common.Net/Listeners/TcpSocketConnectionListener.cs
@@ -0,0 +1,22 @@
+﻿namespace System.Net.Listeners
+{
+    public class TcpSocketConnectionListener : ConnectionListener
+    {
+        private readonly IPEndPoint ipEndPoint;
+
+        public TcpSocketConnectionListener(IPEndPoint ipEndPoint)
+        {
+            this.ipEndPoint = ipEndPoint;
+        }
+
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
