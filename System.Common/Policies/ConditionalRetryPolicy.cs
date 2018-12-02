7a2de92e367535011ea1629f3cad6b7477b72c82
Lapiniot <aossss@gmail.com>
2018-12-02 18:51:56 +0200

    minor code reformatting

--- System.Common/Policies/ConditionalRetryPolicy.cs
+++ System.Common/Policies/ConditionalRetryPolicy.cs
@@ -6,7 +6,7 @@
     {
         private readonly RetryConditionHandler[] conditions;
 
-        public ConditionalRetryPolicy() { }
+        public ConditionalRetryPolicy() {}
 
         public ConditionalRetryPolicy(params RetryConditionHandler[] conditions)
         {
