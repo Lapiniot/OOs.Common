83a1bf42ecd1ada500ef2332c7f8887efe0ca6fc
Lapiniot <aossss@gmail.com>
2018-12-06 22:12:04 +0200

    Segment and SequenceEnumerator have been moved to the System.Common

---
+++ System.Common/Memory/Segment.cs
@@ -0,0 +1,19 @@
+﻿using System.Buffers;
+
+namespace System.Memory
+{
+    public class Segment<T> : ReadOnlySequenceSegment<T>
+    {
+        public Segment(T[] array)
+        {
+            Memory = array;
+        }
+
+        public Segment<T> Append(T[] array)
+        {
+            var segment = new Segment<T>(array) {RunningIndex = RunningIndex + Memory.Length};
+            Next = segment;
+            return segment;
+        }
+    }
+}
---
+++ System.Common/Memory/SequenceEnumerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Buffers;
+
+namespace System.Memory
+{
+    public ref struct SequenceEnumerator<T>
+    {
+        private ReadOnlySequence<T> sequence;
+        private SequencePosition sequencePosition;
+        private int index;
+        private ReadOnlySpan<T> span;
+
+        public SequenceEnumerator(ReadOnlySequence<T> sequence)
+        {
+            this.sequence = sequence;
+            sequencePosition = sequence.Start;
+            index = 0;
+            span = default;
+        }
+
+        public bool MoveNext()
+        {
+            if(index == -1) return false;
+
+            if(index < span.Length - 1)
+            {
+                index++;
+                return true;
+            }
+
+            if(sequence.TryGet(ref sequencePosition, out var memory))
+            {
+                span = memory.Span;
+                index = 0;
+                return true;
+            }
+
+            index = -1;
+            return false;
+        }
+
+        public void Reset()
+        {
+            span = default;
+            index = 0;
+        }
+
+        public ref readonly T Current => ref span[index];
+    }
+}
