c01797b884ed0d0085baadc0e975775cc6146157
Lapiniot <aossss@gmail.com>
2018-12-17 17:34:02 +0200

    Missing C# 8.0 related staff

---
+++ System.Common/AsyncEnumeratorCompilerFixes.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks.Sources;
+
+namespace System.Threading.Tasks
+{
+    public struct ManualResetValueTaskSourceLogic<TResult>
+    {
+        private ManualResetValueTaskSourceCore<TResult> core;
+        public ManualResetValueTaskSourceLogic(IStrongBox<ManualResetValueTaskSourceLogic<TResult>> parent) : this() {}
+        public short Version => core.Version;
+
+        public TResult GetResult(short token)
+        {
+            return core.GetResult(token);
+        }
+
+        public ValueTaskSourceStatus GetStatus(short token)
+        {
+            return core.GetStatus(token);
+        }
+
+        public void OnCompleted(Action<object> continuation, object state, short token, ValueTaskSourceOnCompletedFlags flags)
+        {
+            core.OnCompleted(continuation, state, token, flags);
+        }
+
+        public void Reset()
+        {
+            core.Reset();
+        }
+
+        public void SetResult(TResult result)
+        {
+            core.SetResult(result);
+        }
+
+        public void SetException(Exception error)
+        {
+            core.SetException(error);
+        }
+    }
+}
+
+namespace System.Runtime.CompilerServices
+{
+    public interface IStrongBox<T>
+    {
+        ref T Value { get; }
+    }
+}
