324a77ac6f0a3723115fb34277d95cabf2f99283
Lapiniot <aossss@gmail.com>
2020-10-14 18:16:46 +0300

    ConditionalRetryPolicy added

---
+++ System.Common/Policies/ConditionalRetryPolicy.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+
+namespace System.Policies
+{
+    public class ConditionalRetryPolicy : RetryPolicy
+    {
+        private readonly IEnumerable<RepeatCondition> conditions;
+
+        public ConditionalRetryPolicy(IEnumerable<RepeatCondition> conditions)
+        {
+            this.conditions = conditions;
+        }
+
+        #region Overrides of RetryPolicy
+
+        protected override bool ShouldRetry(Exception exception, int attempt, TimeSpan totalTime, ref TimeSpan delay)
+        {
+            foreach(var condition in conditions)
+            {
+                if(!condition(exception, attempt, totalTime, ref delay)) return false;
+            }
+
+            return true;
+        }
+
+        #endregion
+    }
+}
