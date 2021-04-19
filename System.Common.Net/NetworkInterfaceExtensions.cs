36097de58a5b70249af0b1efc42c76615880011b
Lapiniot <aossss@gmail.com>
2021-04-20 01:25:06 +0300

    Interfaces renamed to NetworkInterfaceExtensions + few more helper methods implemented

---
+++ System.Common.Net/NetworkInterfaceExtensions.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using static System.Net.NetworkInformation.NetworkInterface;
+using static System.Net.NetworkInformation.OperationalStatus;
+using static System.Net.Sockets.AddressFamily;
+
+namespace System.Net
+{
+    public static class NetworkInterfaceExtensions
+    {
+        public static NetworkInterface FindBestMulticastInterface()
+        {
+            return GetAllNetworkInterfaces().FirstOrDefault(i => IsActiveMulticastEnabled(i))
+                ?? throw new InvalidOperationException("No valid network interface with multicast support found.");
+        }
+
+        public static int GetIndex(this NetworkInterface networkInterface, AddressFamily addressFamily)
+        {
+            if(networkInterface is null) throw new ArgumentNullException(nameof(networkInterface));
+
+            return addressFamily switch
+            {
+                InterNetwork => networkInterface.GetIPProperties().GetIPv4Properties().Index,
+                InterNetworkV6 => networkInterface.GetIPProperties().GetIPv6Properties().Index,
+                _ => throw new ArgumentException("Unsupported address family")
+            };
+        }
+
+        public static IPAddress GetPrimaryAddress(this NetworkInterface networkInterface, AddressFamily addressFamily)
+        {
+            if(networkInterface is null) throw new ArgumentNullException(nameof(networkInterface));
+
+            return networkInterface.GetIPProperties().UnicastAddresses.
+                FirstOrDefault(a => a.Address.AddressFamily == addressFamily).Address;
+        }
+
+        public static bool IsActiveMulticastEnabled(this NetworkInterface networkInterface)
+        {
+            if(networkInterface is null) throw new ArgumentNullException(nameof(networkInterface));
+
+            return networkInterface.GetIPProperties().GatewayAddresses.Count > 0 &&
+                   networkInterface.SupportsMulticast &&
+                   networkInterface.OperationalStatus == Up;
+        }
+
+        public static NetworkInterface FindInterface(string adapterNameOrAddress)
+        {
+            if(IPAddress.TryParse(adapterNameOrAddress, out var address))
+            {
+                return NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault(i => i.GetIPProperties().UnicastAddresses.Any(ua => ua.Address.Equals(address)));
+            }
+            else
+            {
+                return NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault(i => i.Name == adapterNameOrAddress);
+            }
+        }
+
+        public static IEnumerable<NetworkInterface> GetActiveExternalInterfaces(this IEnumerable<NetworkInterface> interfaces)
+        {
+            return interfaces.Where(ni =>
+                ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet &&
+                ni.OperationalStatus == OperationalStatus.Up &&
+                ni.GetIPProperties().GatewayAddresses.Count > 0);
+        }
+
+        public static IPAddress FindExternalIPv4Address(this IEnumerable<NetworkInterface> interfaces)
+        {
+            return interfaces.FirstOrDefault(i => i.Supports(NetworkInterfaceComponent.IPv4))?.GetIPProperties()
+                .UnicastAddresses.FirstOrDefault(a => a.Address.AddressFamily == InterNetwork)?.Address;
+        }
+
+        public static IPAddress FindExternalIPv6Address(this IEnumerable<NetworkInterface> interfaces)
+        {
+            return interfaces.FirstOrDefault(i => i.Supports(NetworkInterfaceComponent.IPv6))?.GetIPProperties()
+                .UnicastAddresses.FirstOrDefault(a => a.Address.AddressFamily == InterNetworkV6)?.Address;
+        }
+    }
+}
