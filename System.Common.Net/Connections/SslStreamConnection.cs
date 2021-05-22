728253e3c1d00fe2c4110c6cf2f1f24846ef8920
Lapiniot <aossss@gmail.com>
2021-05-22 19:41:32 +0300

    SslStreamTcpSocketListener draft

---
+++ System.Common.Net/Connections/SslStreamConnection.cs
@@ -0,0 +1,47 @@
+using System.Net.Security;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Net.Connections
+{
+    public sealed class SslStreamConnection : NetworkConnection
+    {
+        private SslStream sslStream;
+        private readonly SslServerAuthenticationOptions options;
+
+        public SslStreamConnection(SslStream sslStream, SslServerAuthenticationOptions options)
+        {
+            this.sslStream = sslStream ?? throw new ArgumentNullException(nameof(sslStream));
+            this.options = options ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        public override ValueTask<int> ReceiveAsync(Memory<byte> buffer, CancellationToken cancellationToken)
+        {
+            return sslStream.ReadAsync(buffer, cancellationToken);
+        }
+
+        public override ValueTask SendAsync(Memory<byte> buffer, CancellationToken cancellationToken)
+        {
+            return sslStream.WriteAsync(buffer, cancellationToken);
+        }
+
+        protected override Task StoppingAsync()
+        {
+            sslStream.Close();
+            return Task.CompletedTask;
+        }
+
+        protected override Task StartingAsync(object state, CancellationToken cancellationToken)
+        {
+            return sslStream.AuthenticateAsServerAsync(options, cancellationToken);
+        }
+
+        public override async ValueTask DisposeAsync()
+        {
+            await using(sslStream.ConfigureAwait(false))
+            {
+                await base.DisposeAsync().ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ System.Common.Net/Listeners/SslStreamTcpSocketListener.cs
@@ -0,0 +1,53 @@
+using System.Net.Connections;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+
+namespace System.Net.Listeners
+{
+    public class SslStreamTcpSocketListener : TcpSocketListenerBase
+    {
+        private readonly SslServerAuthenticationOptions options;
+
+        public SslStreamTcpSocketListener(IPEndPoint endPoint,
+            X509Certificate serverCertificate, int backlog = 100,
+            Action<Socket> configureListening = null,
+            Action<Socket> configureAccepted = null) :
+            base(endPoint, backlog, configureListening, configureAccepted)
+        {
+            options = new SslServerAuthenticationOptions()
+            {
+                ServerCertificate = serverCertificate ?? throw new ArgumentNullException(nameof(serverCertificate))
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(SslStreamTcpSocketListener)} {{tcps://{EndPoint}}}";
+        }
+
+        protected override INetworkConnection CreateConnection(Socket acceptedSocket)
+        {
+            var stream = new NetworkStream(acceptedSocket, true);
+
+            try
+            {
+                var sslStream = new SslStream(stream, false);
+                try
+                {
+                    return new SslStreamConnection(sslStream, options);
+                }
+                catch
+                {
+                    sslStream.Dispose();
+                    throw;
+                }
+            }
+            catch
+            {
+                stream.Dispose();
+                throw;
+            }
+        }
+    }
+}
