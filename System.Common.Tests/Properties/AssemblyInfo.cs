0865b70996e7ba80ccfe148bf0348e845d615eec
Lapiniot <aossss@gmail.com>
2021-11-13 15:51:28 +0200

    reverted last commit as <CLSCompliant> property doesn't work as expected

--- System.Common.Tests/Properties/AssemblyInfo.cs
+++ System.Common.Tests/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ global using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 [assembly: Parallelize(Workers = 4, Scope = ExecutionScope.MethodLevel)]
+[assembly: CLSCompliant(true)]
--- System.Common/Properties/AssemblyInfo.cs
+++ System.Common/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("System.Common.Tests")]
+[assembly: CLSCompliant(true)]
