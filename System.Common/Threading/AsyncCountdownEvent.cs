eb77513e6f1afe5c1f7a59228d5ed63520c22ae6
Lapiniot <aossss@gmail.com>
2022-05-04 17:49:11 +0300

    AsyncCountdownEvent prototype draft

---
+++ System.Common/Threading/AsyncCountdownEvent.cs
@@ -0,0 +1,27 @@
+﻿#nullable enable
+namespace System.Threading;
+
+public class AsyncCountdownEvent
+{
+    private int signals;
+
+    public AsyncCountdownEvent(int signalCount)
+    {
+        Verify.ThrowIfLessThan(signalCount, 0);
+        signals = signalCount;
+    }
+
+    public void AddCount() => AddCount(1);
+
+    public void AddCount(int signalCount) => Interlocked.Add(ref signals, signalCount);
+
+    public void Signal() => Interlocked.Decrement(ref signals);
+
+    public void Signal(int signalCount) => Interlocked.Add(ref signals, -signalCount);
+
+    public Task WaitAsync(CancellationToken cancellationToken = default) => throw new NotImplementedException();
+
+    public void Reset() => throw new NotImplementedException();
+
+    public void Reset(int signalCount) => throw new NotImplementedException();
+}
