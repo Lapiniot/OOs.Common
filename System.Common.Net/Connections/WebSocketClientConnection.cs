e126a4e1d1aab8ed7d700daf8789d6837b32880e
Lapiniot <aossss@gmail.com>
2022-12-12 19:12:49 +0200

    Custom HttpMessageInvoker instance can be provided to ClientWebSocket for CONNECT

--- System.Common.Net/Connections/WebSocketClientConnection.cs
+++ System.Common.Net/Connections/WebSocketClientConnection.cs
@@ -6,14 +6,16 @@ namespace System.Net.Connections;
 public sealed class WebSocketClientConnection : WebSocketConnection<ClientWebSocket>
 {
     private readonly Action<ClientWebSocketOptions> configureOptions;
+    private readonly HttpMessageInvoker invoker;
 
-    public WebSocketClientConnection(Uri remoteUri, Action<ClientWebSocketOptions> configureOptions)
+    public WebSocketClientConnection(Uri remoteUri, Action<ClientWebSocketOptions> configureOptions, HttpMessageInvoker invoker)
         : base(null)
     {
         ArgumentNullException.ThrowIfNull(remoteUri);
 
         RemoteUri = remoteUri;
         this.configureOptions = configureOptions;
+        this.invoker = invoker;
     }
 
     public Uri RemoteUri { get; }
@@ -24,7 +26,7 @@ public sealed class WebSocketClientConnection : WebSocketConnection<ClientWebSoc
         {
             var ws = Socket = new();
             configureOptions?.Invoke(ws.Options);
-            await ws.ConnectAsync(RemoteUri, cancellationToken).ConfigureAwait(false);
+            await ws.ConnectAsync(RemoteUri, invoker, cancellationToken).ConfigureAwait(false);
         }
         catch (WebSocketException wse) when (
             wse.InnerException is HttpRequestException { InnerException: SocketException { SocketErrorCode: SocketError.HostNotFound } })
