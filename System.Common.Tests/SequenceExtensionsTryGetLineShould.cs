1adeb658fb9131ae0e118a2ddc03be71d7a6ed39
Lapiniot <aossss@gmail.com>
2023-03-08 13:25:15 +0200

    SequenceFactory helper introduced

--- System.Common.Tests/SequenceExtensionsTryGetLineShould.cs
+++ System.Common.Tests/SequenceExtensionsTryGetLineShould.cs
@@ -30,9 +30,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnTrueAndLineGivenValidContiguousSequence()
     {
-        var segment = new MemorySegment<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33, (byte)'\r', (byte)'\n' });
-        var sequence = new ReadOnlySequence<byte>(segment, 0,
-            segment.Append(new byte[] { 0x33, 0x22, 0x11, 0x00 }), 4);
+        var sequence = SequenceFactory.Create<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33, (byte)'\r', (byte)'\n' }, new byte[] { 0x33, 0x22, 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
@@ -48,10 +46,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnTrueAndLineGivenValidFragmentedSequenceCase1()
     {
-        var segment = new MemorySegment<byte>(new byte[] { 0x00, 0x11 });
-        var sequence = new ReadOnlySequence<byte>(segment, 0, segment
-            .Append(new byte[] { 0x22, 0x33, (byte)'\r', (byte)'\n' })
-            .Append(new byte[] { 0x33, 0x22, 0x11, 0x00 }), 4);
+        var sequence = SequenceFactory.Create<byte>(new byte[] { 0x00, 0x11 }, new byte[] { 0x22, 0x33, (byte)'\r', (byte)'\n' }, new byte[] { 0x33, 0x22, 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
@@ -67,10 +62,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnTrueAndLineGivenValidFragmentedSequenceCase2()
     {
-        var segment = new MemorySegment<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33 });
-        var sequence = new ReadOnlySequence<byte>(segment, 0, segment
-            .Append(new[] { (byte)'\r', (byte)'\n' })
-            .Append(new byte[] { 0x33, 0x22, 0x11, 0x00 }), 4);
+        var sequence = SequenceFactory.Create<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33 }, new[] { (byte)'\r', (byte)'\n' }, new byte[] { 0x33, 0x22, 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
@@ -86,10 +78,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnTrueAndLineGivenValidFragmentedSequenceCase3()
     {
-        var segment = new MemorySegment<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33 });
-        var sequence = new ReadOnlySequence<byte>(segment, 0, segment
-            .Append(new[] { (byte)'\r' })
-            .Append(new byte[] { (byte)'\n', 0x33, 0x22, 0x11, 0x00 }), 4);
+        var sequence = SequenceFactory.Create<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33 }, new[] { (byte)'\r' }, new byte[] { (byte)'\n', 0x33, 0x22, 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
@@ -105,10 +94,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnTrueAndLineGivenValidFragmentedSequenceCase4()
     {
-        var segment = new MemorySegment<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33, (byte)'\r' });
-        var sequence = new ReadOnlySequence<byte>(segment, 0, segment
-            .Append(new byte[] { (byte)'\n', 0x33, 0x22 })
-            .Append(new byte[] { 0x11, 0x00 }), 2);
+        var sequence = SequenceFactory.Create<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33, (byte)'\r' }, new byte[] { (byte)'\n', 0x33, 0x22 }, new byte[] { 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
@@ -124,9 +110,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnTrueAndLineGivenValidFragmentedSequenceCase5()
     {
-        var segment = new MemorySegment<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33, (byte)'\r', (byte)'\n', 0x33, 0x22 });
-        var sequence = new ReadOnlySequence<byte>(segment, 0, segment
-            .Append(new byte[] { 0x11, 0x00 }), 2);
+        var sequence = SequenceFactory.Create<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33, (byte)'\r', (byte)'\n', 0x33, 0x22 }, new byte[] { 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
@@ -142,11 +126,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnFalseAndEmptyLineGivenInvalidSingleSegmentSequenceCase1()
     {
-        var sequence = new ReadOnlySequence<byte>(new byte[]
-        {
-            0x00, 0x11, 0x22, 0x33,
-            0x33, 0x22, 0x11, 0x00
-        });
+        var sequence = new ReadOnlySequence<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33, 0x33, 0x22, 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
@@ -157,12 +137,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnFalseAndEmptyLineGivenInvalidSingleSegmentSequenceCase2()
     {
-        var sequence = new ReadOnlySequence<byte>(new byte[]
-        {
-            0x00, 0x11, 0x22, 0x33,
-            (byte)'\r', 0x00,
-            0x33, 0x22, 0x11, 0x00
-        });
+        var sequence = new ReadOnlySequence<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33, (byte)'\r', 0x00, 0x33, 0x22, 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
@@ -173,12 +148,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnFalseAndEmptyLineGivenInvalidSingleSegmentSequenceCase3()
     {
-        var sequence = new ReadOnlySequence<byte>(new byte[]
-        {
-            0x00, 0x11, 0x22, 0x33,
-            (byte)'\n', 0x00,
-            0x33, 0x22, 0x11, 0x00
-        });
+        var sequence = new ReadOnlySequence<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33, (byte)'\n', 0x00, 0x33, 0x22, 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
@@ -189,12 +159,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnFalseAndEmptyLineGivenInvalidSingleSegmentSequenceCase4()
     {
-        var sequence = new ReadOnlySequence<byte>(new byte[]
-        {
-            0x00, 0x11, 0x22, 0x33,
-            (byte)'\r', 0x00, (byte)'\n',
-            0x33, 0x22, 0x11, 0x00
-        });
+        var sequence = new ReadOnlySequence<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33, (byte)'\r', 0x00, (byte)'\n', 0x33, 0x22, 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
@@ -205,9 +170,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnFalseAndEmptyLineGivenInvalidContiguousSequence()
     {
-        var segment = new MemorySegment<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33 });
-        var sequence = new ReadOnlySequence<byte>(segment, 0,
-            segment.Append(new byte[] { 0x33, 0x22, 0x11, 0x00 }), 4);
+        var sequence = SequenceFactory.Create<byte>(new byte[] { 0x00, 0x11, 0x22, 0x33 }, new byte[] { 0x33, 0x22, 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
@@ -218,10 +181,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnFalseAndEmptyLineGivenInvalidFragmentedSequenceCase1()
     {
-        var segment = new MemorySegment<byte>(new byte[] { 0x00, 0x11 });
-        var sequence = new ReadOnlySequence<byte>(segment, 0, segment
-            .Append(new byte[] { 0x22, 0x33, (byte)'\r', 0x00, (byte)'\n' })
-            .Append(new byte[] { 0x33, 0x22, 0x11, 0x00 }), 4);
+        var sequence = SequenceFactory.Create<byte>(new byte[] { 0x00, 0x11 }, new byte[] { 0x22, 0x33, (byte)'\r', 0x00, (byte)'\n' }, new byte[] { 0x33, 0x22, 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
@@ -232,10 +192,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnFalseAndEmptyLineGivenInvalidFragmentedSequenceCase2()
     {
-        var segment = new MemorySegment<byte>(new byte[] { 0x00, 0x11 });
-        var sequence = new ReadOnlySequence<byte>(segment, 0, segment
-            .Append(new byte[] { 0x22, 0x33, 0x00, (byte)'\n' })
-            .Append(new byte[] { 0x33, 0x22, 0x11, 0x00 }), 4);
+        var sequence = SequenceFactory.Create<byte>(new byte[] { 0x00, 0x11 }, new byte[] { 0x22, 0x33, 0x00, (byte)'\n' }, new byte[] { 0x33, 0x22, 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
@@ -246,10 +203,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnFalseAndEmptyLineGivenInvalidFragmentedSequenceCase3()
     {
-        var segment = new MemorySegment<byte>(new byte[] { 0x00, 0x11 });
-        var sequence = new ReadOnlySequence<byte>(segment, 0, segment
-            .Append(new byte[] { 0x22, 0x33, (byte)'\r', 0x00 })
-            .Append(new byte[] { 0x33, 0x22, 0x11, 0x00 }), 4);
+        var sequence = SequenceFactory.Create<byte>(new byte[] { 0x00, 0x11 }, new byte[] { 0x22, 0x33, (byte)'\r', 0x00 }, new byte[] { 0x33, 0x22, 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
@@ -260,10 +214,7 @@ public class SequenceExtensionsTryGetLineShould
     [TestMethod]
     public void ReturnFalseAndEmptyLineGivenInvalidFragmentedSequenceCase4()
     {
-        var segment = new MemorySegment<byte>(new byte[] { 0x00, 0x11 });
-        var sequence = new ReadOnlySequence<byte>(segment, 0, segment
-            .Append(new byte[] { 0x22, 0x33, (byte)'\n', (byte)'\r' })
-            .Append(new byte[] { 0x33, 0x22, 0x11, 0x00 }), 4);
+        var sequence = SequenceFactory.Create<byte>(new byte[] { 0x00, 0x11 }, new byte[] { 0x22, 0x33, (byte)'\n', (byte)'\r' }, new byte[] { 0x33, 0x22, 0x11, 0x00 });
 
         var actual = sequence.TryReadLine(out var line);
 
--- System.Common/Memory/MemorySegment.cs
+++ System.Common/Memory/MemorySegment.cs
@@ -6,10 +6,17 @@ public class MemorySegment<T> : ReadOnlySequenceSegment<T>
 {
     public MemorySegment(ReadOnlyMemory<T> memory) => Memory = memory;
 
-    public MemorySegment<T> Append(ReadOnlyMemory<T> memory)
+    public MemorySegment<T> Add(ReadOnlyMemory<T> memory)
     {
         var segment = new MemorySegment<T>(memory) { RunningIndex = RunningIndex + Memory.Length };
         Next = segment;
         return segment;
+    }
+
+    public static MemorySegment<T> operator +(MemorySegment<T> left, ReadOnlyMemory<T> right)
+    {
+        ArgumentNullException.ThrowIfNull(left);
+
+        return left.Add(right);
     }
 }
