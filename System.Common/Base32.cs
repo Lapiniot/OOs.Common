e0133ff07ae8dfd62cc2a3ce182c2568076ba157
Lapiniot <aossss@gmail.com>
2023-04-09 12:47:05 +0300

    Base32: performance optimization (~25% boost)

--- System.Common/Base32.cs
+++ System.Common/Base32.cs
@@ -4,31 +4,28 @@ namespace System;
 
 public static class Base32
 {
-    private static readonly char[] Alphabet =
-    {
-        '0', '1', '2', '3', '4', '5', '6', '7',
-        '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
-        'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V'
-    };
-
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string ToBase32String(long value) => string.Create(13, value, WriteBuffer);
 
     private static void WriteBuffer(Span<char> span, long value)
     {
-        span[12] = Alphabet[(int)(value & 31)];
-        span[11] = Alphabet[(int)((value >> 5) & 31)];
-        span[10] = Alphabet[(int)((value >> 10) & 31)];
-        span[9] = Alphabet[(int)((value >> 15) & 31)];
-        span[8] = Alphabet[(int)((value >> 20) & 31)];
-        span[7] = Alphabet[(int)((value >> 25) & 31)];
-        span[6] = Alphabet[(int)((value >> 30) & 31)];
-        span[5] = Alphabet[(int)((value >> 35) & 31)];
-        span[4] = Alphabet[(int)((value >> 40) & 31)];
-        span[3] = Alphabet[(int)((value >> 45) & 31)];
-        span[2] = Alphabet[(int)((value >> 50) & 31)];
-        span[1] = Alphabet[(int)((value >> 55) & 31)];
-        span[0] = Alphabet[(int)((value >> 60) & 31)];
+        // this fixed size array will be referenced directly from 
+        // data section of dll as a part of no-alloc optimization
+        var alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUV"u8;
+
+        // write in backward order to eliminate redundant bounds checks
+        span[12] = (char)alphabet[(int)(value & 31)];
+        span[11] = (char)alphabet[(int)((value >>> 5) & 31)];
+        span[10] = (char)alphabet[(int)((value >>> 10) & 31)];
+        span[9] = (char)alphabet[(int)((value >>> 15) & 31)];
+        span[8] = (char)alphabet[(int)((value >>> 20) & 31)];
+        span[7] = (char)alphabet[(int)((value >>> 25) & 31)];
+        span[6] = (char)alphabet[(int)((value >>> 30) & 31)];
+        span[5] = (char)alphabet[(int)((value >>> 35) & 31)];
+        span[4] = (char)alphabet[(int)((value >>> 40) & 31)];
+        span[3] = (char)alphabet[(int)((value >>> 45) & 31)];
+        span[2] = (char)alphabet[(int)((value >>> 50) & 31)];
+        span[1] = (char)alphabet[(int)((value >>> 55) & 31)];
+        span[0] = (char)alphabet[(int)((value >>> 60) & 31)];
     }
 }
