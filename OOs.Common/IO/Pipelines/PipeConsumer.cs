4a1d7bda8d493c0e2d493e86d03629a5423a420f
Lapiniot <aossss@gmail.com>
2024-03-02 10:53:14 +0200

    PipeConsumer should be able to reuse abortTokenSource unless cancellation has been requested

--- OOs.Common/IO/Pipelines/PipeConsumer.cs
+++ OOs.Common/IO/Pipelines/PipeConsumer.cs
@@ -1,5 +1,7 @@
 using System.IO.Pipelines;
 
+#nullable enable
+
 namespace OOs.IO.Pipelines;
 
 /// <summary>
@@ -14,37 +16,47 @@ public abstract class PipeConsumer : PipeConsumerCore
     {
         ArgumentNullException.ThrowIfNull(reader);
         this.reader = reader;
+        abortTokenSource = new();
     }
 
-    protected Task ConsumerCompletion { get; private set; }
+    protected Task? ConsumerCompletion { get; private set; }
 
     protected CancellationToken Aborted => abortTokenSource.Token;
 
     protected override Task StartingAsync(CancellationToken cancellationToken)
     {
-        abortTokenSource ??= new();
+        ResetAbortTokenSource();
         ConsumerCompletion = RunConsumerAsync(reader, abortTokenSource.Token);
         return Task.CompletedTask;
     }
 
     protected override async Task StoppingAsync()
     {
-        using (abortTokenSource)
-        {
-            abortTokenSource = null;
-            // Try to perform graceful consumer loop cancellation first of all, 
-            // by cancelling current pending reader.ReadAsync() call  
-            reader.CancelPendingRead();
+        // Try to perform graceful consumer loop cancellation first of all, 
+        // by cancelling current pending reader.ReadAsync() call  
+        reader.CancelPendingRead();
 
-            try
+        try
+        {
+            await ConsumerCompletion!.ConfigureAwait(false);
+        }
+        catch (OperationCanceledException) { /* expected */ }
+    }
+
+    private void ResetAbortTokenSource()
+    {
+        if (!abortTokenSource.TryReset())
+        {
+            using (abortTokenSource)
             {
-                await ConsumerCompletion.ConfigureAwait(false);
+                abortTokenSource = new();
             }
-            catch (OperationCanceledException) { /* expected */ }
         }
     }
 
-    protected void Abort() => abortTokenSource?.Cancel();
+    protected void Abort() => abortTokenSource.Cancel();
+
+    protected void AbortAsync() => abortTokenSource.CancelAsync();
 
     public override async ValueTask DisposeAsync()
     {
