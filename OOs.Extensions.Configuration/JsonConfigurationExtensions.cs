bbeb08047eb9b29223ed0e9e5fa2dbbbd738581d
Lapiniot <aossss@gmail.com>
2024-06-17 10:51:48 +0300

    JsonConfigurationExtensions.AddPlatformSpecificJsonFile introduced

---
+++ OOs.Extensions.Configuration/JsonConfigurationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using OS = System.OperatingSystem;
+
+namespace OOs.Extensions.Configuration;
+
+public static class JsonConfigurationExtensions
+{
+    public static IConfigurationBuilder AddPlatformSpecificJsonFile(this IConfigurationBuilder builder, bool optional, bool reloadOnChange)
+    {
+        var platform = OS.IsWindows()
+            ? "Windows"
+            : OS.IsLinux()
+                ? "Linux"
+                : OS.IsFreeBSD()
+                    ? "FreeBSD"
+                    : OS.IsMacOS() || OS.IsMacCatalyst()
+                        ? "MacOS"
+                        : "UnknownOS";
+
+        return builder.AddJsonFile($"appsettings.{platform}.json", optional, reloadOnChange);
+    }
+}
