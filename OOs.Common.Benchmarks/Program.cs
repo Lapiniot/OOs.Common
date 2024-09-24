7347c5b4e81cd103e5c625d18696bf2e6c16c0af
Lapiniot <aossss@gmail.com>
2024-09-24 21:51:22 +0300

    OOs.Common.Benchmarks: provide global configuration with job defaults preconfigured that can be overridden from cmdline Fixes #7

--- OOs.Common.Benchmarks/Program.cs
+++ OOs.Common.Benchmarks/Program.cs
@@ -1,22 +1,9 @@
-﻿BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config: BuildGlobalConfig());
-
-internal sealed partial class Program
-{
-    private static ManualConfig BuildGlobalConfig()
-    {
-        var config = ManualConfig.CreateMinimumViable()
-            .WithOption(ConfigOptions.DisableLogFile, true)
-            .WithOption(ConfigOptions.LogBuildOutput, false)
-            .WithOption(ConfigOptions.GenerateMSBuildBinLog, false)
-            .WithSummaryStyle(SummaryStyle.Default.WithRatioStyle(RatioStyle.Percentage));
-
-        config.AddJob(Job.Default
-            .WithArguments([new MsBuildArgument("/p:UseArtifactsOutput=false")])
-        // .WithEnvironmentVariable(new EnvironmentVariable("DOTNET_JitDisasm", "System.Net.Http.Base64UrlSafe:EncodeToUtf8"))
-        // .WithEnvironmentVariable(new EnvironmentVariable("DOTNET_JitDiffableDasm", "1"))
-        // .WithEnvironmentVariable(new EnvironmentVariable("DOTNET_JitStdOutFile", $"{Environment.CurrentDirectory}/EncodeToUtf8.asm"))
-        );
-
-        return config;
-    }
-}
+﻿BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config: DefaultConfig.Instance
+    .WithOption(ConfigOptions.StopOnFirstError, true)
+    .WithOption(ConfigOptions.DisableLogFile, true)
+    .WithOption(ConfigOptions.LogBuildOutput, false)
+    .WithOption(ConfigOptions.GenerateMSBuildBinLog, false)
+    .WithSummaryStyle(SummaryStyle.Default.WithRatioStyle(RatioStyle.Percentage))
+    .AddJob(Job.Default
+        .WithArguments([new MsBuildArgument("/p:UseArtifactsOutput=false")])
+        .AsDefault()));
