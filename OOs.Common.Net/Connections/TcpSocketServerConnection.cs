6f0d0927f0a7ca923b8ff9073e98ff2f1781244f
Lapiniot <aossss@gmail.com>
2025-02-19 15:54:26 +0200

    Remove all NetworkConnection based implementations as obsolete Close #17

--- OOs.Common.Net/Connections/TcpSocketServerConnection.cs
+++
@@ -1,9 +0,0 @@
-using System.Net.Sockets;
-
-namespace OOs.Net.Connections;
-
-[Obsolete("Consider usage of OOs.Net.Connections.ServerTcpSocketTransportConnection instead.")]
-public sealed class TcpSocketServerConnection(Socket acceptedSocket) : SocketConnection(acceptedSocket, false)
-{
-    protected override Task StartingAsync(CancellationToken cancellationToken) => Task.CompletedTask;
-}
