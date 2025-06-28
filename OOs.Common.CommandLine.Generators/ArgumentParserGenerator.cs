0936ffc53d68fb37cf308187d58cacdbc33d4708
Lapiniot <aossss@gmail.com>
2025-06-28 21:09:04 +0300

    Add support for generator disablement via project build property Close #35

--- OOs.Common.CommandLine.Generators/ArgumentParserGenerator.cs
+++ OOs.Common.CommandLine.Generators/ArgumentParserGenerator.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Immutable;
 using System.Text;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 
 namespace OOs.Common.CommandLine.Generators;
@@ -9,108 +8,145 @@ namespace OOs.Common.CommandLine.Generators;
 [Generator]
 public class ArgumentParserGenerator : IIncrementalGenerator
 {
+    private const string Prefix = "build_property.";
+    private const string EnableOptionName = "EnableArgumentParserGenerator";
+    private const string ClassNameOptionName = "ArgumentParserGeneratorClassName";
+    private const string NamespaceOptionName = "ArgumentParserGeneratorNamespace";
+    private const string RootNamespaceName = "RootNamespace";
+
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        var attributes = context.SyntaxProvider.ForAttributeWithMetadataName<SourceData>("OOs.CommandLine.OptionAttribute`1",
-            predicate: (node, _) => node is
-                CompilationUnitSyntax { AttributeLists.Count: > 0 } or
-                ClassDeclarationSyntax { AttributeLists.Count: > 0 } or
-                StructDeclarationSyntax { AttributeLists.Count: > 0 },
-            transform: (ctx, cancellationToken) =>
-            {
-                var builder = ImmutableArray.CreateBuilder<OptionData>();
-                foreach (var attr in ctx.Attributes)
-                {
-                    if (cancellationToken.IsCancellationRequested)
-                    {
-                        break;
-                    }
-
-                    if (attr is
-                        {
-                            ConstructorArguments: [{ Value: string name }, { Value: string longAlias }, .. var cargs],
-                            NamedArguments: var nargs,
-                            AttributeClass:
-                            {
-                                IsGenericType: true,
-                                TypeArguments: [{ SpecialType: { } type }]
-                            }
-                        })
-                    {
-                        var shortAlias = cargs is [{ Value: char sa }] ? sa : '\0';
-                        string? description = null;
-                        string? hint = null;
-
-                        foreach (var item in nargs)
-                        {
-                            switch (item)
-                            {
-                                case { Key: "ShortAlias", Value.Value: char s }:
-                                    shortAlias = s; break;
-                                case { Key: "Description", Value.Value: string d }:
-                                    description = d; break;
-                                case { Key: "Hint", Value.Value: string h }:
-                                    hint = h; break;
-                            }
-                        }
-
-                        builder.Add(new(name, longAlias, shortAlias, type, description, hint));
-                    }
-                }
-
-                var arguments = builder.ToImmutable();
-
-                var generateSynopsis = false;
-                foreach (var item in ctx.TargetSymbol.GetAttributes())
-                {
-                    if (item is
-                        {
-                            AttributeClass:
-                            {
-                                Name: "ArgumentParserGenerationOptionsAttribute",
-                                ContainingNamespace:
-                                {
-                                    Name: "CommandLine",
-                                    ContainingNamespace:
-                                    {
-                                        Name: "OOs",
-                                        ContainingNamespace.IsGlobalNamespace: true
-                                    }
-                                }
-                            },
-                            NamedArguments: var args
-                        })
-                    {
-                        generateSynopsis = args.FirstOrDefault(x =>
-                            x.Key == "GenerateSynopsis" &&
-                            x.Value.Type?.SpecialType == SpecialType.System_Boolean).Value.Value is true;
-                        break;
-                    }
-                }
-
-                return ctx.TargetSymbol is ITypeSymbol
-                {
-                    Name: var typeName,
-                    ContainingNamespace: var cns,
-                    DeclaredAccessibility: var accessibility,
-                    TypeKind: var kind,
-                }
-                    ? new(typeName, cns.ToDisplayString(), kind, accessibility, arguments, generateSynopsis)
-                    : new("ArgumentParser", "OOs.CommandLine.Generated",
-                        Kind: TypeKind.Class, Accessibility: Accessibility.Public, arguments, generateSynopsis);
-
-            }).WithComparer(EqualityComparer<SourceData>.Default);
-
-        context.RegisterSourceOutput(attributes, static (ctx, source) =>
+        var optionProvider = context.AnalyzerConfigOptionsProvider.Select(static (options, _) =>
         {
-            if (source.Options.Length is 0)
+            var globalOptions = options.GlobalOptions;
+            return new GeneratorOptions(
+                IsEnabled: !globalOptions.TryGetValue($"{Prefix}{EnableOptionName}", out var value) ||
+                    bool.TryParse(value, out var enabled) && enabled,
+                ClassName: globalOptions.TryGetValue($"{Prefix}{ClassNameOptionName}", out value) && !string.IsNullOrEmpty(value)
+                    ? value
+                    : "Arguments",
+                NamespaceName: globalOptions.TryGetValue($"{Prefix}{NamespaceOptionName}", out value) && !string.IsNullOrEmpty(value)
+                    ? value
+                    : globalOptions.TryGetValue($"{Prefix}{RootNamespaceName}", out value) && !string.IsNullOrEmpty(value)
+                        ? value
+                        : "OOs.CommandLine.Generated");
+        });
+
+        var sourceDataProvider = context.SyntaxProvider.ForAttributeWithMetadataName(
+                fullyQualifiedMetadataName: "OOs.CommandLine.OptionAttribute`1",
+                predicate: static (_, _) => true,
+                transform: ToSourceData)
+            .WithComparer(EqualityComparer<SourceData>.Default)
+            .WithTrackingName($"{nameof(ArgumentParserGenerator)}SourceData");
+
+        var combined = optionProvider.Combine(sourceDataProvider.Collect());
+
+        context.RegisterSourceOutput(combined, static (ctx, source) =>
+        {
+            var (defaults, sources) = source;
+            if (!defaults.IsEnabled)
             {
                 return;
             }
 
-            var code = ArgumentParserCodeEmitter.Emit(source.Namespace, source.Name, source.Kind,
-                source.Accessibility, source.Options, source.GenerateSynopsis);
-            ctx.AddSource($"{source.Namespace}.{source.Name}.g.cs", SourceText.From(code, Encoding.UTF8));
+            foreach (var (name, ns, kind, accessibility, options, genSynopsis) in sources)
+            {
+                if (options.Length is 0)
+                {
+                    continue;
+                }
+
+                var namespaceName = ns ?? defaults.NamespaceName;
+                var typeName = name ?? defaults.ClassName;
+                var code = ArgumentParserCodeEmitter.Emit(namespaceName, typeName, kind, accessibility, options, genSynopsis);
+                ctx.AddSource($"{namespaceName}.{typeName}.g.cs", SourceText.From(code, Encoding.UTF8));
+            }
         });
     }
+
+    private static SourceData ToSourceData(GeneratorAttributeSyntaxContext ctx, CancellationToken cancellationToken)
+    {
+        var builder = ImmutableArray.CreateBuilder<OptionData>();
+        foreach (var attr in ctx.Attributes)
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                break;
+            }
+
+            if (attr is
+                {
+                    ConstructorArguments: [{ Value: string name }, { Value: string longAlias }, .. var cargs],
+                    NamedArguments: var nargs,
+                    AttributeClass:
+                    {
+                        IsGenericType: true,
+                        TypeArguments: [{ SpecialType: { } type }]
+                    }
+                })
+            {
+                var shortAlias = cargs is [{ Value: char sa }] ? sa : '\0';
+                string? description = null;
+                string? hint = null;
+
+                foreach (var item in nargs)
+                {
+                    switch (item)
+                    {
+                        case { Key: "ShortAlias", Value.Value: char s }:
+                            shortAlias = s; break;
+                        case { Key: "Description", Value.Value: string d }:
+                            description = d; break;
+                        case { Key: "Hint", Value.Value: string h }:
+                            hint = h; break;
+                    }
+                }
+
+                builder.Add(new(name, longAlias, shortAlias, type, description, hint));
+            }
+        }
+
+        var arguments = builder.ToImmutable();
+
+        var generateSynopsis = false;
+        foreach (var item in ctx.TargetSymbol.GetAttributes())
+        {
+            if (item is
+                {
+                    AttributeClass:
+                    {
+                        Name: "ArgumentParserGenerationOptionsAttribute",
+                        ContainingNamespace:
+                        {
+                            Name: "CommandLine",
+                            ContainingNamespace:
+                            {
+                                Name: "OOs",
+                                ContainingNamespace.IsGlobalNamespace: true
+                            }
+                        }
+                    },
+                    NamedArguments: var args
+                })
+            {
+                generateSynopsis = args.FirstOrDefault(static x =>
+                    x.Key == "GenerateSynopsis" &&
+                    x.Value.Type?.SpecialType == SpecialType.System_Boolean).Value.Value is true;
+                break;
+            }
+        }
+
+        return ctx.TargetSymbol is ITypeSymbol
+        {
+            Name: var typeName,
+            ContainingNamespace: var cns,
+            DeclaredAccessibility: var accessibility,
+            TypeKind: var kind,
+        }
+            ? new(typeName, cns.ToDisplayString(), kind, accessibility, arguments, generateSynopsis)
+            : new(null, null,
+                Kind: TypeKind.Class, Accessibility: Accessibility.Public, arguments, generateSynopsis);
+    }
 }
+
+public record struct GeneratorOptions(bool IsEnabled, string ClassName, string NamespaceName);
--- OOs.Common.CommandLine.Generators/SourceData.cs
+++ OOs.Common.CommandLine.Generators/SourceData.cs
@@ -3,7 +3,7 @@ using Microsoft.CodeAnalysis;
 
 namespace OOs.Common.CommandLine.Generators;
 
-public record struct SourceData(string Name, string Namespace, TypeKind Kind,
+public record struct SourceData(string? Name, string? Namespace, TypeKind Kind,
     Accessibility Accessibility, ImmutableArray<OptionData> Options, bool GenerateSynopsis) :
     IEquatable<SourceData>
 {
@@ -11,8 +11,8 @@ public record struct SourceData(string Name, string Namespace, TypeKind Kind,
     public bool Equals(SourceData other)
 #pragma warning restore IDE0251 // Make member 'readonly'
     {
-        return EqualityComparer<string>.Default.Equals(Name, other.Name) &&
-            EqualityComparer<string>.Default.Equals(Namespace, other.Namespace) &&
+        return EqualityComparer<string?>.Default.Equals(Name, other.Name) &&
+            EqualityComparer<string?>.Default.Equals(Namespace, other.Namespace) &&
             EqualityComparer<TypeKind>.Default.Equals(Kind, other.Kind) &&
             EqualityComparer<Accessibility>.Default.Equals(Accessibility, other.Accessibility) &&
             EqualityComparer<bool>.Default.Equals(GenerateSynopsis, other.GenerateSynopsis) &&
@@ -22,8 +22,8 @@ public record struct SourceData(string Name, string Namespace, TypeKind Kind,
     public override readonly int GetHashCode()
     {
         return HashCode.Combine(
-            EqualityComparer<string>.Default.GetHashCode(Name),
-            EqualityComparer<string>.Default.GetHashCode(Namespace),
+            EqualityComparer<string?>.Default.GetHashCode(Name),
+            EqualityComparer<string?>.Default.GetHashCode(Namespace),
             EqualityComparer<TypeKind>.Default.GetHashCode(Kind),
             EqualityComparer<Accessibility>.Default.GetHashCode(Accessibility),
             EqualityComparer<bool>.Default.GetHashCode(GenerateSynopsis),
